b54eac5221bafd3990302644ab3d311264d6e9e3
timmypelumy <timileyinpelumi.dev@gmail.com>
2023-08-28 17:22:07 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,4 +10,9 @@ export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server",
   adapter: vercel(),
+  vite: {
+    ssr: {
+      noExternal: ["react-icons"],
+    },
+  },
 });
