d49ef4c27d803e88ea5639b676ae2e7cfa2ca209
timmypelumy <timileyinpelumi.dev@gmail.com>
2023-08-28 17:21:10 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
 import vercel from "@astrojs/vercel/serverless";
-s;
 
 // https://astro.build/config
 export default defineConfig({
