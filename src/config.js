cfd5163668873d30a20963f9fedf2af49e66b55a
timmypelumy <timileyinpelumi.dev@gmail.com>
2023-08-28 17:34:25 +0100

    init

--- src/config.js
+++ src/config.js
@@ -3,7 +3,8 @@ const config = {
   description:
     "Moon Crypto is a cryptocurrency broker based in the United States. With a focus on simplicity, Moon Crypto allows you to buy and sell Bitcoin and other cryptocurrencies instantly and easily.",
 
-  apiUrl: `${import.meta.env.API_URL}/api` || "http://localhost:3000/api",
+  apiUrl:
+    `https://eccentric-eight.vercel.app/api` || "http://localhost:3000/api",
   // apiUrl: "http://localhost:3000/api",
 
   authCookieKey: "AUTH_USER_ECCENTRIC",
