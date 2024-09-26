1e0dbcc103a9f79e1bac8a9d935c4170b3d14e6c
timmypelumy <timmypelumy@gmail.com>
2024-09-26 21:29:16 +0100

    updates to portfolio

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
-  appName: "𝕏-Crypto Brokerage",
+  appName: "EmuskFX",
   description:
-    "Moon Crypto is a cryptocurrency broker based in the United States. With a focus on simplicity, Moon Crypto allows you to buy and sell Bitcoin and other cryptocurrencies instantly and easily.",
+    "EmuskFX Crypto is a cryptocurrency broker based in the United States. With a focus on simplicity, Moon Crypto allows you to buy and sell Bitcoin and other cryptocurrencies instantly and easily.",
 
   apiUrl: import.meta.env.PUBLIC_API_URL || "http://localhost:3001/api",
 
